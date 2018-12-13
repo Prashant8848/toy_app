5639172332c6a75d4d7c407663e4086f375229e2
“Prashant8848” <prashant.shrestha8848@gmail.com>
2018-12-13 15:03:38 +0900

    making changes to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181109021850) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20181108114723) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
